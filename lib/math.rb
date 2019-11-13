ffd3269205494e3b0096a44478ec5645470244da
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-11-13 20:44:33 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,36 @@
 def addition(num1, num2)
-
+addition adds two numbers together
+addition = num1 + num2
+puts addition
 end
-
+addition(1, 5)
 def subtraction(num1, num2)
-
+  subtraction subtracts two numbers from each other
+subtraction = 'num1 - num2'
+puts subtraction
 end
-
+subtraction(9, 1)
 def division(num1, num2)
-
+  division divides one number by another
+division = 'num1 / num2'
+puts division
 end
-
+division(20, 2)
 def multiplication(num1, num2)
-
+  multiplication multiplies two numbers toghers
+multiplication = 'num1 * num2'
+puts multiplication
 end
-
+multiplication(2, 2)
 def modulo(num1, num2)
-
+  modulo returns the remainder of two numbers
+modulo = 'num1 / num2'
+puts modulo
 end
-
+modulo(21, 2)
 def square_root(num)
-
+  square_root returns the square root of a number
+  puts Math.sqrt(num)
 end
+square_root(9)
+
